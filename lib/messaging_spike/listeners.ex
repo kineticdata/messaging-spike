82cb78e9df59815f03030b32b86221c57a32aae6
Chad Rehm <chad.rehm@kineticdata.com>
2022-07-05 15:38:17 -0500

    Updated rpc handler

--- lib/messaging_spike/listeners.ex
+++ lib/messaging_spike/listeners.ex
@@ -6,7 +6,7 @@ defmodule MessagingSpike.Listeners do
 
     Rabbit.subscribe("faq", fn message, meta ->
       question = Map.get(:erlang.binary_to_term(message), "question")
-      answer = "Shayne"
+      answer = :random.uniform(100)
 
       reply_to = Map.get(meta, :reply_to)
       delivery_tag = Map.get(meta, :delivery_tag)
