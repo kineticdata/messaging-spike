3a1d26e76d49f8c090199c373a94d1851c58aa22
Shayne Koestler <shayne.koestler@kineticdata.com>
2022-07-05 15:01:56 -0500

    partial work commit

--- lib/messaging_spike/brokers/rabbit.ex
+++ lib/messaging_spike/brokers/rabbit.ex
@@ -7,6 +7,23 @@ defmodule MessagingSpike.Brokers.Rabbit do
     GenServer.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
+  def rpc(topic, payload) do
+    spawn(fn ->
+      pid = self()
+      correlation_id = erlang.unique_integer |> :erlang.integer_to_binary() |> Base.encode64()
+
+      GenServer.call(__MODULE__, {:rpc_publish, MY_PID})
+
+      receive do
+        {:message_type, value} ->
+          nil
+          # code
+      end
+    end)
+
+    ...(wait(here(for response)))
+  end
+
   def publish(topic, payload) do
     GenServer.call(__MODULE__, {:publish, topic, payload})
   end
@@ -27,6 +44,10 @@ defmodule MessagingSpike.Brokers.Rabbit do
     GenServer.call(__MODULE__, {:declare, topic})
   end
 
+  def ack(delivery_tag) do
+    GenServer.call(__MODULE__, {:ack, delivery_tag})
+  end
+
   # Callbacks
 
   def init(_init_arg) do
@@ -41,14 +62,29 @@ defmodule MessagingSpike.Brokers.Rabbit do
     {:ok, conn} = AMQP.Connection.open("amqp://#{host}:#{port}")
     {:ok, chan} = AMQP.Channel.open(conn)
 
-    {:ok, {chan}}
+    {:ok, %{queue: reply_queue}} =
+      AMQP.Queue.declare(chan, "", exclusive: true, auto_delete: true)
+
+    {:ok} = AMQP.Queue.subscribe(chan, topic, fun)
+
+    {:ok, {chan, reply_queue, %{}}}
   end
 
-  def handle_call(command, _from, {chan}) do
+  def handle_call(command, _from, {chan, reply_queue, pid_map}) do
+    new_pid_map = pid_map
+
     result =
       case command do
         {:publish, topic, payload} ->
-          AMQP.Basic.publish(chan, topic, "", payload)
+          AMQP.Basic.publish(chan, "", topic, payload)
+
+        {:rpc_publish, topic, payload, correlation_id, pid} ->
+          new_pid_map = Map.put(pid_map, correlation_id, pid)
+
+          AMQP.Basic.publish(chan, "", topic, payload,
+            correlation_id: correlation_id,
+            reply_to: reply_queue
+          )
 
         {:dequeue, topic} ->
           AMQP.Basic.get(chan, topic)
@@ -60,11 +96,12 @@ defmodule MessagingSpike.Brokers.Rabbit do
           AMQP.Queue.subscribe(chan, topic, fun)
 
         {:declare, topic} ->
-          AMQP.Exchange.declare(chan, topic, :direct, durable: true)
           AMQP.Queue.declare(chan, topic, durable: true)
-          AMQP.Queue.bind(chan, topic, topic)
+
+        {:ack, delivery_tag} ->
+          AMQP.Basic.ack(chan, delivery_tag)
       end
 
-    {:reply, result, {chan}}
+    {:reply, result, {chan, reply_queue, new_pid_map}}
   end
 end
